297b491853c931d3f744a70f377d8331bd8fa150
Gabriel-Reis021 <gabrielreissantos180@gmail.com>
2025-08-10 23:57:17 -0300

    chore: adicionando novo username, password, mysql database name para realizar deploy

--- process/conn.php
+++ process/conn.php
@@ -1,17 +1,17 @@
 <?php
-    session_start();
+session_start();
 
-    $user = "root";
-    $pass = "";
-    $db ="pizzaria";
-    $host = "localhost";
+$user = "if0_39677648";
+$pass = "gabrielelibni12";
+$db = "if0_39677648_pizzaria";
+$host = "sql105.infinityfree.com";
 
-    try{
-        $conn = new PDO("mysql:host={$host};dbname={$db}", $user, $pass);
-        $conn ->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $conn ->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-    }catch(PDOException $e){
-        print "Erro " . $e->getMessage(). "<br/>";
-        die();
-    }
+try {
+   $conn = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $conn->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+} catch(PDOException $e) {
+    print "Erro: " . $e->getMessage() . "<br/>";
+    die();
+}
 ?>
